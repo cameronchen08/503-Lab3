34dcae847402ca165025acc74d5ca4daa6ad430d
Cameron Chen <cameronchen08@gmail.com>
2025-05-14 11:36:22 -0700

    declared main

--- lab3.cpp
+++ lab3.cpp
@@ -24,14 +24,51 @@
 #include <sys/time.h>
 using namespace std;
 
+// maintain start and finish wall time
 struct timeval start, end2;
+
 void startTimer() {
     gettimeofday(&start, NULL);
 }
+
 void stopTimer(const char *str) {
     gettimeofday(&end2, NULL);
     cout << str << "'s elpased time\t= " << ((end2.tv_sec - start.tv_sec) * 1000000 + (end2.tv_usec - start.tv_usec)) << endl;
 }
+
 int main (int argc, char *argv[]) {
-    
+    if (argc != 3) {
+        cerr << "usage: lab3 filename bytes" << endl;
+        return -1;
+    }
+    int bytes = atoi(argv[2]);
+    if (bytes < 1) {
+        cerr << "usage: lab3 filename bytes" << endl;
+        cerr << "where bytes > 0" << endl;
+        return -1;
+    }
+    char *filename = argv[1];
+    char *buf = new char[bytes];
+
+    // Linux I/O
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        cerr << filename << "not found" << endl;
+        return -1;
+    }
+    startTimer();
+    while (read(fd, buf, bytes) > 0) {
+        stopTimer("Unix read");
+        close(fd);
+    }
+
+    // standard I/O
+
+    // write the same functionality as in Linux I/O
+
+    // but use fopen(), fgetc(), fread(), and fclose()
+
+    // use fgetc() if bytes == 1
+
+    return 0;
 } 
